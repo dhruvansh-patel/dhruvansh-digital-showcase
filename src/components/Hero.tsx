8fb20170099438e85076d3f71b5ff05e2615af61
Dhruvansh Patel <75726789+dhruvansh-patel@users.noreply.github.com>
2025-04-08 18:01:25 -0600

    Fixing hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -131,7 +131,7 @@ const Hero: React.FC = () => {
               
               <div className="mb-1.5 pl-6"><span className="text-white">);</span></div>
               
-              <div className="mb-1.5"><span className="text-white">};</span></div>
+              <div className="mb-1.5"><span className="text-white">;</span></div>
               
               <div className="mb-1.5"><span className="text-purple-400">export</span> <span className="text-purple-400">default</span> <span className="text-yellow-300">Portfolio</span>;</div>
             </div>
